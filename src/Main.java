f53874c0c82beb9809c5d892c46075abf3a85f7a
MishkaMO <mishkamo@yandex.ru>
2022-11-05 13:42:05 +0300

    Разница веса

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        var weghtBoxer1 = 78.2;
-        System.out.println(weghtBoxer1);
-        var weghtBoxer2 = 82.7;
-        System.out.println(weghtBoxer2);
-        var totalWeghtBoxer = weghtBoxer1 + weghtBoxer2;
+        var weightBoxer1 = 78.2;
+        System.out.println(weightBoxer1);
+        var weightBoxer2 = 82.7;
+        System.out.println(weightBoxer2);
+        var totalWeghtBoxer = weightBoxer1 + weightBoxer2;
         System.out.println ("общий вес бойцов " + totalWeghtBoxer + "кг " );
-        var weightDifference = weghtBoxer2 - weghtBoxer1;
+        var weightDifference = weightBoxer1 - weightBoxer2;
         System.out.println("Разница веса бойцов " + weightDifference + "кг ");
-        var divisionRemainder = (weghtBoxer2 + weghtBoxer1) % weightDifference;
+
+        var divisionRemainder = (weightBoxer2 + weightBoxer1) % weightBoxer1;
         System.out.println("Разница веса " + divisionRemainder + "кг ");
     }
 }
