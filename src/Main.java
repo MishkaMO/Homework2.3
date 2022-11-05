21815782221271bf270f8ca95cdeafdcc5d8c23e
MishkaMO <mishkamo@yandex.ru>
2022-11-05 12:10:56 +0300

    Разница веса

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println(weghtBoxer2);
         var totalWeghtBoxer = weghtBoxer1 + weghtBoxer2;
         System.out.println ("общий вес бойцов " + totalWeghtBoxer + "кг " );
+        var weightDifference = weghtBoxer2 - weghtBoxer1;
+        System.out.println("Разница веса бойцов " + weightDifference + "кг ");
     }
 }
