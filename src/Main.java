9402758b9e7582bf991e3d15123d4adc7bdb5ff7
MishkaMO <mishkamo@yandex.ru>
2022-11-05 14:33:50 +0300

    Решение по задаче

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
         System.out.println(workHour);
         var workers = totalHour / workHour;
         System.out.println("Всего работников в компании - " + workers + " человек ");
+        var newWorkers = workers + 94;
+        System.out.println(newWorkers);
+        var worksTotal = totalHour / newWorkers;
+        System.out.println("Если в компании работает " + newWorkers + " человек, то всего " + worksTotal + " часов работы может быть поделено между сотрудниками");
     }
 }
