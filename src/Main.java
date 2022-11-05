2e1a669f54e82f5d1b686b3ec489e90e64367b4d
MishkaMO <mishkamo@yandex.ru>
2022-11-05 11:57:24 +0300

    общий вес бойцов

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var weghtBoxer1 = 78.2;
+        System.out.println(weghtBoxer1);
+        var weghtBoxer2 = 82.7;
+        System.out.println(weghtBoxer2);
+        var totalWeghtBoxer = weghtBoxer1 + weghtBoxer2;
+        System.out.println ("общий вес бойцов " + totalWeghtBoxer + "кг " );
+    }
+}
